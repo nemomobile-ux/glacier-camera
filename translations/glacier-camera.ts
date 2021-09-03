ed9a5cd23ca4cd99de583df5bc344d109beb2857
Jozef Mlich <jmlich83@gmail.com>
2021-09-03 13:49:26 +0000

    lupdate

--- translations/glacier-camera.ts
+++ translations/glacier-camera.ts
@@ -1,12 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ru_RU">
+<TS version="2.1">
 <context>
     <name>CameraPage</name>
     <message>
         <location filename="../src/qml/pages/CameraPage.qml" line="17"/>
         <source>Camera</source>
-        <translation>Камера</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="../src/qml/pages/PreviewPage.qml" line="15"/>
         <source>Preview</source>
-        <translation>Предпросмотр</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -22,7 +22,7 @@
     <message>
         <location filename="../src/main.cpp" line="43"/>
         <source>Camera</source>
-        <translation>Камера</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -30,7 +30,7 @@
     <message>
         <location filename="../src/qml/pages/SettingsPage.qml" line="15"/>
         <source>Settings</source>
-        <translation>Настройки</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/qml/pages/SettingsPage.qml" line="42"/>
